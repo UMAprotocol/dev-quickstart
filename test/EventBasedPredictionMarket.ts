db922072a30c4039e96c8412aab09424fa753618
Pablo Maldonado <pablomaldonadoturci@gmail.com>
2022-07-04 02:41:14 -0700

    fix: fix interfaces and change ancillary question (#3)
    Signed-off-by: Pablo Maldonado <pablo@umaproject.org>
    
    Co-authored-by: Pablo Maldonado <pablo@umaproject.org>

--- test/EventBasedPredictionMarket.ts
+++ test/EventBasedPredictionMarket.ts
@@ -1,13 +1,13 @@
 import { MIN_INT_VALUE } from "@uma/common";
-import { OptimisticOracle } from "@uma/contracts-node/dist/packages/contracts-node/typechain/core/ethers";
-import { EventBasedPredictionMarket, ExpandedERC20, OptimisticOracleV2 } from "../typechain";
+import { OptimisticOracleV2Ethers } from "@uma/contracts-node";
+import { EventBasedPredictionMarket, ExpandedERC20 } from "../typechain";
 import { amountToSeedWallets } from "./constants";
 import { eventBasedPredictionMarketFixture } from "./fixtures/EventBasedPredictionMarket.Fixture";
 import { umaEcosystemFixture } from "./fixtures/UmaEcosystem.Fixture";
 import { BigNumber, Contract, ethers, expect, SignerWithAddress, toWei } from "./utils";
 
 let eventBasedPredictionMarket: EventBasedPredictionMarket, usdc: Contract;
-let optimisticOracle: OptimisticOracleV2, longToken: ExpandedERC20, shortToken: ExpandedERC20;
+let optimisticOracle: OptimisticOracleV2Ethers, longToken: ExpandedERC20, shortToken: ExpandedERC20;
 let deployer: SignerWithAddress, sponsor: SignerWithAddress, holder: SignerWithAddress, disputer: SignerWithAddress;
 
 describe("EventBasedPredictionMarket functions", function () {
