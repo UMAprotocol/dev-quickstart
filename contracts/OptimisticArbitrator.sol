8c2c15b499a06a2bf4df5dc3d557df549c9ad6ef
Pablo Maldonado <pablo@umaproject.org>
2022-10-11 12:54:53 +0100

    feat: update versions
    Signed-off-by: Pablo Maldonado <pablo@umaproject.org>

--- contracts/OptimisticArbitrator.sol
+++ contracts/OptimisticArbitrator.sol
@@ -4,15 +4,15 @@ pragma solidity ^0.8.16;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-import "../../interfaces/StoreInterface.sol";
-import "../../interfaces/OracleAncillaryInterface.sol";
-import "../../interfaces/FinderInterface.sol";
-import "../../interfaces/IdentifierWhitelistInterface.sol";
-import "../../interfaces/OptimisticOracleV2Interface.sol";
+import "@uma/core/contracts/oracle/interfaces/StoreInterface.sol";
+import "@uma/core/contracts/oracle/interfaces/OracleAncillaryInterface.sol";
+import "@uma/core/contracts/oracle/interfaces/FinderInterface.sol";
+import "@uma/core/contracts/oracle/interfaces/IdentifierWhitelistInterface.sol";
+import "@uma/core/contracts/oracle/interfaces/OptimisticOracleV2Interface.sol";
 
-import "../../../common/implementation/AncillaryData.sol";
-import "../../../common/implementation/AddressWhitelist.sol";
-import "../Constants.sol";
+import "@uma/core/contracts/common/implementation/AncillaryData.sol";
+import "@uma/core/contracts/common/implementation/AddressWhitelist.sol";
+import "@uma/core/contracts/oracle/implementation/Constants.sol";
 
 contract OptimisticArbitrator {
     using SafeERC20 for IERC20;
---
+++ test/fixtures/OptimisticArbitrator.Fixture.ts
@@ -0,0 +1,59 @@
+// import hre from "hardhat";
+// import Web3 from "web3";
+// import { EventBasedPredictionMarket, ExpandedERC20 } from "../../typechain";
+// import { TokenRolesEnum } from "../constants";
+// import { getContractFactory, SignerWithAddress, BigNumber, Contract } from "../utils";
+// import { umaEcosystemFixture } from "./UmaEcosystem.Fixture";
+// const { utf8ToHex } = Web3.utils;
+
+// export const optimisticArbitratorFixture = hre.deployments.createFixture(async ({ ethers }) => {
+//   return await deployOptimisticArbitrator(ethers);
+// });
+
+// export async function deployOptimisticArbitrator(ethers: Ethers) {
+//   const [deployer] = await ethers.getSigners();
+
+//   // This fixture is dependent on the UMA ecosystem fixture. Run it first and grab the output. This is used in the
+//   // deployments that follows. The output is spread when returning contract instances from this fixture.
+//   const parentFixture = await umaEcosystemFixture();
+
+//   // deploys collateral for EventBasedPredictionMarket contract
+//   const usdc = await (await getContractFactory("ExpandedERC20", deployer)).deploy("USD Coin", "USDC", 6);
+//   await usdc.addMember(TokenRolesEnum.MINTER, deployer.address);
+
+//   // Sets collateral as approved in the UMA collateralWhitelist.
+//   await parentFixture.collateralWhitelist.addToWhitelist(usdc.address);
+
+//   // Deploy the EventBasedPredictionMarket contract.
+//   const eventBasedPredictionMarket = (await (
+//     await getContractFactory("EventBasedPredictionMarket", deployer)
+//   ).deploy(
+//     "XYZ",
+//     usdc.address,
+//     utf8ToHex("when X happens, will the price of Y equal Z?"),
+//     parentFixture.finder.address,
+//     parentFixture.timer.address
+//   )) as EventBasedPredictionMarket;
+
+//   // connect to existing ExpandedERC20 contract with ether
+//   const longToken = (await (
+//     await getContractFactory("ExpandedERC20", deployer)
+//   ).attach(await eventBasedPredictionMarket.longToken())) as ExpandedERC20;
+//   const shortToken = (await (
+//     await getContractFactory("ExpandedERC20", deployer)
+//   ).attach(await eventBasedPredictionMarket.shortToken())) as ExpandedERC20;
+
+//   return { ...parentFixture, usdc, eventBasedPredictionMarket, deployer, longToken, shortToken };
+// }
+
+// export async function seedAndApprove(
+//   accounts: SignerWithAddress[],
+//   collateral: Contract,
+//   amount: BigNumber,
+//   approvedTarget: string
+// ) {
+//   for (const account of accounts) {
+//     await collateral.mint(account.address, amount);
+//     await collateral.connect(account).approve(approvedTarget, amount);
+//   }
+// }
