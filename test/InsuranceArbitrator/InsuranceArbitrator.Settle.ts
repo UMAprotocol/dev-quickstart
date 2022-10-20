1a65a3197c72aa122d709699ec5f26ca1a19e9ee
Reinis Martinsons <reinis@umaproject.org>
2022-10-20 18:14:25 +0000

    test: settle after liveness no dispute
    Signed-off-by: Reinis Martinsons <reinis@umaproject.org>

--- test/InsuranceArbitrator/InsuranceArbitrator.Settle.ts
+++ test/InsuranceArbitrator/InsuranceArbitrator.Settle.ts
@@ -17,7 +17,7 @@ let deployer: SignerWithAddress,
   claimant: SignerWithAddress,
   disputer: SignerWithAddress,
   settler: SignerWithAddress;
-let policyId: string;
+let policyId: string, claimId: string;
 let expectedAncillaryData: string;
 let expectedBond: BigNumber;
 let requestTime: BigNumber, expectedExpirationTime: BigNumber;
@@ -49,7 +49,8 @@ describe("Insurance Arbitrator: Settle", function () {
 
     // Submit claim and grab required request details for interacting with Optimistic Oracle.
     requestTime = await timer.getCurrentTime();
-    await insuranceArbitrator.connect(claimant).submitClaim(policyId);
+    const submitClaimTx = insuranceArbitrator.connect(claimant).submitClaim(policyId);
+    claimId = await getClaimIdFromTx(insuranceArbitrator, submitClaimTx);
     expectedAncillaryData = constructAncillaryData(insuredEvent);
     expectedExpirationTime = await getExpirationTime(insuranceArbitrator, requestTime);
   });
@@ -59,5 +60,33 @@ describe("Insurance Arbitrator: Settle", function () {
         .connect(settler)
         .settle(insuranceArbitrator.address, identifier, requestTime, expectedAncillaryData)
     ).to.be.reverted;
+  });
+  it("Settle after liveness without dispute", async function () {
+    const insuredBalanceBefore = await usdc.balanceOf(insured.address);
+    const contractBalanceBefore = await usdc.balanceOf(insuranceArbitrator.address);
+    const claimantBalanceBefore = await usdc.balanceOf(claimant.address);
+    const ooBalanceBefore = await usdc.balanceOf(optimisticOracle.address);
+
+    // Advance time post liveness and settle through Optimistic Oracle.
+    await optimisticOracle.setCurrentTime(expectedExpirationTime);
+    const settleTx = optimisticOracle
+      .connect(settler)
+      .settle(insuranceArbitrator.address, identifier, requestTime, expectedAncillaryData);
+
+    // Verify emitted transaction log.
+    await expect(settleTx).to.emit(insuranceArbitrator, "ClaimAccepted").withArgs(claimId, policyId);
+
+    // Verify insured amount has been paid and bond returned.
+    expect(await usdc.balanceOf(insured.address)).to.equal(insuredBalanceBefore.add(insuredAmount));
+    expect(await usdc.balanceOf(insuranceArbitrator.address)).to.equal(contractBalanceBefore.sub(insuredAmount));
+    expect(await usdc.balanceOf(claimant.address)).to.equal(claimantBalanceBefore.add(expectedBond));
+    expect(await usdc.balanceOf(optimisticOracle.address)).to.equal(ooBalanceBefore.sub(expectedBond));
+
+    // Repeated claim on paid out insurance should not be possible.
+    await usdc.connect(deployer).mint(claimant.address, expectedBond);
+    await usdc.connect(claimant).approve(insuranceArbitrator.address, expectedBond);
+    await expect(insuranceArbitrator.connect(claimant).submitClaim(policyId)).to.be.revertedWith(
+      "Insurance not issued"
+    );
   });
 });
